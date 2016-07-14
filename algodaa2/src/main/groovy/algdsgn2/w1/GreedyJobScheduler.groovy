662e23f89948cd2aaa849640398fce98be00caca
ESTRADA Nicolas <nicolas.estrada@orange.com>
2016-07-14 03:55:58 +0200

    Cleaned up, still needs some javadoc

--- algodaa2/src/main/groovy/algdsgn2/w1/GreedyJobScheduler.groovy
+++ algodaa2/src/main/groovy/algdsgn2/w1/GreedyJobScheduler.groovy
@@ -3,47 +3,54 @@ package algdsgn2.w1
 import groovy.transform.CompileStatic
 import groovy.transform.ToString
 
+import static java.util.Arrays.sort
+
 @CompileStatic
 class GreedyJobScheduler {
 
-    Job[] jobs
-    int[] weightedCompletionTimes
+    static final DIFF_HEURISTIC = { Job o1, Job o2 ->
+        def diff1 = o1.weight - o1.length
+        def diff2 = o2.weight - o2.length
+        def result = Integer.compare(diff2, diff1)
+        result != 0 ? result : Integer.compare(o2.weight, o1.weight)
+    }
 
-    GreedyJobScheduler(URL url) {
-        url.withReader {
-            def scanner = new Scanner(it)
-            def len = scanner.nextInt()
+    static final RATIO_HEURISTIC = { Job o1, Job o2 ->
+        def diff1 = (o1.weight / o1.length) as double
+        def diff2 = (o2.weight / o2.length) as double
+        def result = Double.compare(diff2, diff1)
+        result != 0 ? result : Integer.compare(o2.weight, o1.weight)
+    }
+
+    Job[] jobs
+
+    GreedyJobScheduler(URL url, heuristic) {
+        parseJobsFile url
+        sort jobs, heuristic as Comparator
+        updateWeightedCompletionTimes()
+    }
+
+    private parseJobsFile(URL url) {
+        url.withReader { reader ->
+            def scanner = new Scanner(reader)
+            int len = scanner.nextInt()
             jobs = new Job[len]
             len.times {
                 jobs[it] = new Job(scanner.nextInt(), scanner.nextInt())
             }
         }
-        Arrays.sort(jobs, new Comparator<Job>() {
-            @Override
-            int compare(final Job o1, final Job o2) {
-                def diff1 = o1.weight - o1.length
-                def diff2 = o2.weight - o2.length
-                def result = Integer.compare(diff1, diff2)
-                if (result == 0) {
-                    return Integer.compare(o1.weight, o2.weight)
-                } else {
-                    return result
-                }
-            }
-        })
-        jobs.reverse(true)
-        def time = 0
-        weightedCompletionTimes = new int[jobs.length]
-        for (int i = 0; i < weightedCompletionTimes.length; i++) {
-            time += jobs[i].length
-            weightedCompletionTimes[i] = jobs[i].weight * time
+    }
+
+    private updateWeightedCompletionTimes() {
+        def totalCompletionTime = 0L
+        jobs.each { Job job ->
+            totalCompletionTime += job.length
+            job.weightedCompletionTime = job.weight * totalCompletionTime
         }
     }
 
     long getTotalWeightedCompletionTime() {
-        weightedCompletionTimes.inject(0L) { long total, int time ->
-            total + time
-        } as long
+        jobs.inject(0L) { total, job -> total + job.weightedCompletionTime } as long
     }
 
     @ToString(includePackage = false)
@@ -51,6 +58,8 @@ class GreedyJobScheduler {
 
         final int weight
         final int length
+
+        long weightedCompletionTime
 
         Job(final int weight, final int length) {
             this.weight = weight
--- algodaa2/src/test/groovy/algdsgn2/w1/GreedyJobSchedulerSpec.groovy
+++ algodaa2/src/test/groovy/algdsgn2/w1/GreedyJobSchedulerSpec.groovy
@@ -3,24 +3,31 @@ package algdsgn2.w1
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import static algdsgn2.w1.GreedyJobScheduler.DIFF_HEURISTIC
+import static algdsgn2.w1.GreedyJobScheduler.RATIO_HEURISTIC
+
 class GreedyJobSchedulerSpec extends Specification {
 
     @Unroll
-    def "it should calculate the sum of weighted completion time #time of the resulting schedule defined in '#file'"() {
+    def '''it should calculate the sum of weighted completion times (using #heuristic)
+           of the job schedule defined in #file'''() {
 
         given:
         def input = GreedyJobSchedulerSpec.class.getResource(file)
-        def scheduler = new GreedyJobScheduler(input)
+        def scheduler = new GreedyJobScheduler(input, heuristic)
 
         expect:
         scheduler.totalWeightedCompletionTime == time
 
         where:
-        file             | time
-        'jobs_test1.txt' | 31814
-        'jobs_test2.txt' | 61545
-        'jobs_test3.txt' | 688647
-        'jobs.txt'       | 69119377652
+        file             | heuristic       | time
+        'jobs_test1.txt' | DIFF_HEURISTIC  | 31814
+        'jobs_test1.txt' | RATIO_HEURISTIC | 31814
+        'jobs_test2.txt' | DIFF_HEURISTIC  | 61545
+        'jobs_test2.txt' | RATIO_HEURISTIC | 60213
+        'jobs_test3.txt' | DIFF_HEURISTIC  | 688647
+        'jobs_test3.txt' | RATIO_HEURISTIC | 674634
+        'jobs.txt'       | DIFF_HEURISTIC  | 69119377652
 
     }
 }
