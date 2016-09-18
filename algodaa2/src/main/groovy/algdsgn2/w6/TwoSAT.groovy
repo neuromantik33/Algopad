7b759478b3cc5744f96d5d8c7ab77e24285b9883
ESTRADA Nicolas <nicolas.estrada@orange.com>
2016-09-19 00:36:58 +0200

    Working however way too slow

---
+++ algodaa2/src/main/groovy/algdsgn2/w6/TwoSAT.groovy
@@ -0,0 +1,93 @@
+/*
+ *  algopad.
+ */
+
+package algdsgn2.w6
+
+import groovy.transform.CompileStatic
+import groovy.transform.Immutable
+
+import static algopad.common.misc.Ints.log2
+import static groovy.transform.TypeCheckingMode.SKIP
+import static groovyx.gpars.GParsPool.withPool
+import static java.lang.Math.abs
+import static java.lang.Math.max
+
+/**
+ * @author Nicolas Estrada.
+ */
+@SuppressWarnings('GroovyBreak')
+@CompileStatic
+class TwoSAT {
+
+    final Random rnd = new Random()
+
+    final int numVars
+    final Clause[] clauses
+
+    TwoSAT(int numVars, Clause[] clauses) {
+        this.numVars = numVars
+        this.clauses = clauses
+    }
+
+    /**
+     * TODO
+     *
+     * @param n
+     * @param clauses
+     * @return
+     */
+    @CompileStatic(SKIP)
+    static boolean isSatisfiable(final int n, final Clause[] clauses) {
+        def ts = new TwoSAT(n, clauses)
+        def maxTries = max(log2(n), 10)
+        boolean satisfied = withPool {
+            (1..<maxTries).findAnyParallel {
+                ts.randomWalk()
+            }
+        }
+        satisfied
+    }
+
+    private boolean randomWalk() {
+        def bits = new BigInteger(numVars, rnd)
+        long maxSteps = 2L * numVars * numVars
+        while (maxSteps > 0) {
+            def unsat = areClausesSatisfied(bits)
+            if (unsat.empty) {
+                println "satisfying bits for $clauses = ${bits.toString(2).padLeft(numVars, '0')}"
+                return true
+            }
+            def clause = unsat[rnd.nextInt(unsat.size())]
+            int nextBit = rnd.nextBoolean() ? clause.v1 : clause.v2
+            bits.flipBit abs(nextBit)
+            maxSteps -= 1
+        }
+        false
+    }
+
+    /**
+     * @return the list of unsatisfied clauses for the given <i>bits</i>.
+     */
+    private Collection<Clause> areClausesSatisfied(final BigInteger bits) {
+        clauses.findAll { !it.evaluate(bits) }
+    }
+
+    @Immutable
+    static class Clause {
+
+        int v1, v2
+
+        boolean evaluate(BigInteger bits) {
+            def val1 = v1 < 0 ? !bits.testBit(-v1) : bits.testBit(v1)
+            def val2 = v2 < 0 ? !bits.testBit(-v2) : bits.testBit(v2)
+            val1 || val2
+        }
+
+        String toString() {
+            def s1 = v1 < 0 ? "¬x(${abs(v1)})" : "x($v1)"
+            def s2 = v2 < 0 ? "¬x(${abs(v2)})" : "x($v2)"
+            "{ $s1 ∨ $s2 }"
+        }
+    }
+}
---
+++ algodaa2/src/test/groovy/algdsgn2/w6/TwoSATSpec.groovy
@@ -0,0 +1,58 @@
+package algdsgn2.w6
+
+import org.junit.Rule
+import org.junit.rules.Stopwatch
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static algdsgn2.w6.TwoSAT.Clause
+import static algdsgn2.w6.TwoSAT.isSatisfiable
+import static java.util.concurrent.TimeUnit.MILLISECONDS
+
+class TwoSATSpec extends Specification {
+
+    @Rule
+    Stopwatch stopwatch = new Stopwatch() {}
+
+    @Unroll
+    def 'it should verify the 2 #sat of #file'() {
+
+        given:
+        def url = TwoSATSpec.class.getResource(file)
+        def clauses = parseClauseFile(url)
+
+        expect:
+        isSatisfiable(clauses.length, clauses) == satisfiable
+
+        cleanup:
+        println "TwoSAT($file) : Time spent ${stopwatch.runtime(MILLISECONDS)}ms"
+
+        where:
+        file                | size   | satisfiable
+        '2sat_test1_ok.txt' | 8      | true
+        '2sat_test2_ko.txt' | 8      | false
+        '2sat_test3_ko.txt' | 4      | false
+        '2sat_test4_ok.txt' | 7      | true
+        '2sat1.txt'         | 100000 | true
+        //        '2sat2.txt'         | 200000  | false
+        //        '2sat3.txt'         | 400000  | true
+        //        '2sat4.txt'         | 600000  | true
+        //        '2sat5.txt'         | 800000  | false
+        //        '2sat6.txt'         | 1000000 | false
+
+        sat = satisfiable ? 'satisfiability' : 'unsatisfiability'
+
+    }
+
+    private static parseClauseFile(final URL url) {
+        url.withReader { reader ->
+            def scanner = new Scanner(reader)
+            def n = scanner.nextInt()
+            def clauses = new Clause[n]
+            n.times {
+                clauses[it] = new Clause(v1: scanner.nextInt(), v2: scanner.nextInt())
+            }
+            clauses
+        }
+    }
+}
