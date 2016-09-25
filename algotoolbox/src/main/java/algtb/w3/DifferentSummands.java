5e8195e897f70f6eff3a62445ac563af45a1b9ee
ESTRADA Nicolas <nicolas.estrada@orange.com>
2016-09-25 17:51:49 +0200

    Done with different summands

--- algotoolbox/src/main/java/algtb/w3/DifferentSummands.java
+++ algotoolbox/src/main/java/algtb/w3/DifferentSummands.java
@@ -1,28 +1,49 @@
+/*
+ *  algopad.
+ */
+
 package algtb.w3;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * @author Nicolas Estrada.
+ */
+@SuppressWarnings("StandardVariableNames")
 public final class DifferentSummands {
 
     private DifferentSummands() {}
 
-    private static List<Integer> optimalSummands(final int n) {
-        return new ArrayList<>(n);
+    static List<Long> calculateOptimalSummands(final long n) {
+        final List<Long> summands = new ArrayList<>(DEFAULT_CAPACITY);
+        long k = n;
+        long j = 1;
+        while (true) {
+            if (k <= 2 * j) {
+                summands.add(k);
+                break;
+            }
+            summands.add(j);
+            k -= j;
+            j += 1;
+        }
+        return summands;
     }
 
     @SuppressWarnings("UseOfSystemOutOrSystemErr")
     public static void main(final String... args) {
         try (final Scanner in = new Scanner(System.in, "UTF-8")) {
-
             final int n = in.nextInt();
-            final List<Integer> summands = optimalSummands(n);
-
+            final List<Long> summands = calculateOptimalSummands(n);
             System.out.println(summands.size());
-            for (final Integer summand : summands) {
+            for (final Long summand : summands) {
                 System.out.print(summand + " ");
             }
         }
     }
+
+    private static final int DEFAULT_CAPACITY = 10;
+
 }
