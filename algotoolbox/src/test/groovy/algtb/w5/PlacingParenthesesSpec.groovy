181d30fb26787f88d223ed01dd8b07c3f5d7ebe2
ESTRADA Nicolas <nicolas.estrada@orange.com>
2016-09-29 15:33:43 +0200

    Done with placing parentheses

---
+++ algotoolbox/src/test/groovy/algtb/w5/PlacingParenthesesSpec.groovy
@@ -0,0 +1,29 @@
+/*
+ *  algopad.
+ */
+
+package algtb.w5
+
+import spock.lang.Narrative
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static algtb.w5.PlacingParentheses.calculateMaximumValue
+
+@Narrative('In this problem, your goal is to add parentheses to a given arithmetic expression to maximize its value.')
+class PlacingParenthesesSpec extends Specification {
+
+    @Unroll
+    def '''it should find the maximum value #value for #exp by specifying the order
+           of applying its arithmetic operations using additional parentheses.'''() {
+
+        expect:
+        calculateMaximumValue(exp) == value
+
+        where:
+        exp           | value
+        '1+5'         | 6
+        '5-8+7*4-8+9' | 200
+
+    }
+}
