3c1b7bd2d65391625bb9397dc93323b07d65dc46
ESTRADA Nicolas <nicolas.estrada@orange.com>
2016-10-11 15:57:35 +0200

    Done with binary matrix

--- geeks/src/test/groovy/algopad/geeks/PuzzlesSpec.groovy
+++ geeks/src/test/groovy/algopad/geeks/PuzzlesSpec.groovy
@@ -216,4 +216,82 @@ class PuzzlesSpec extends Specification {
         'AAABABAA'   | 'AABA'  | 128   | [0, 1, 4]
 
     }
+
+    @Unroll
+    @See('http://www.geeksforgeeks.org/shortest-path-in-a-binary-maze')
+    @SuppressWarnings('GroovyOverlyLongMethod')
+    def '''given a #size matrix where each element is either 0 or 1, it should find the shortest
+           path #distance between a given source cell #src to a destination cell #dest.
+           The path can only be created out of a cell if its value is 1.'''() {
+
+        given:
+        matrix = matrix as int[][]
+
+        and:
+        def findShortestPathInMaze = { int[][] matrix, List src, List dest ->
+
+            def m = matrix.length
+            def n = matrix[0].length
+            def distances = new Double[m][n]
+            def infinity = Double.POSITIVE_INFINITY
+            def offsets = [
+              [-1, 0], /* UP */
+              [0, 1], /* RIGHT */
+              [1, 0], /* DOWN */
+              [0, -1] /* LEFT */
+            ]
+
+            def eachAdjacentCell = { List coord, Closure closure ->
+                for (offset in offsets) {
+                    def x = coord[0] + offset[0]
+                    def y = coord[1] + offset[1]
+                    def validX = x >= 0 && x < m
+                    def validY = y >= 0 && y < n
+                    if (validX && validY && matrix[x][y] == 1) {
+                        closure.call x, y
+                    }
+                }
+            }
+
+
+            Queue<List> queue = [] as LinkedList
+            distances[src[0]][src[1]] = 0
+            queue.offer src
+
+            while (!queue.empty) {
+                def coord = queue.poll()
+                def dist = distances[coord[0]][coord[1]]
+                if (coord == dest) { break }
+                eachAdjacentCell(coord) { int x, int y ->
+                    if (distances[x][y] == null) {
+                        distances[x][y] = dist + 1
+                        queue.offer([x, y])
+                    }
+                }
+            }
+
+            distances[dest[0]][dest[1]] ?: infinity
+
+        }
+
+        expect:
+        findShortestPathInMaze(matrix, src, dest) == distance
+
+        where:
+        src    | dest   | distance
+        [0, 0] | [3, 4] | 11
+        [0, 0] | [8, 9] | Double.POSITIVE_INFINITY
+
+        matrix = [[1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+                  [1, 0, 1, 0, 1, 1, 1, 0, 1, 1],
+                  [1, 1, 1, 0, 1, 1, 0, 1, 0, 1],
+                  [0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+                  [1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
+                  [1, 0, 1, 1, 1, 1, 0, 1, 0, 0],
+                  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                  [1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+                  [1, 1, 0, 0, 0, 0, 1, 0, 0, 1]]
+        size = "${matrix.size()}x${matrix[0].size()}"
+
+    }
 }
